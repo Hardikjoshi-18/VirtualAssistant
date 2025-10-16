a4113ee455c406266b527cc6938f49f11a5491e0
HardikJoshi18 <hardikaimnda@gmail.com>
2025-10-16 17:32:45 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -29,8 +29,8 @@ export const signUp = async (req,res) =>{
         res.cookie("token",token,{
             httpOnly:true,
             maxAge:7*24*60*60*1000,
-            sameSite:"strict",
-            secure:false
+            sameSite:"None",
+            secure:true
         })
 
 
@@ -64,8 +64,8 @@ export const login = async (req,res) =>{
         res.cookie("token",token,{
             httpOnly:true,
             maxAge:7*24*60*60*1000,
-            sameSite:"strict",
-            secure:false
+            sameSite:"None",
+            secure:true
         })
 
 
